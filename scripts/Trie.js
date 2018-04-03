6fd8ec3f699a59a30d6cb9c63aff032d70d9b412
meloncatty <k.handel66@gmail.com>
2018-04-03 13:12:18 -0600

    add gitignore

--- scripts/Trie.js
+++ scripts/Trie.js
@@ -3,5 +3,18 @@ import TrieNode from './TrieNode'
 export default class Trie {
   constructor() {
     this.root = new Node()
+
+  }
+
+  insert(word) {
+
+  }
+
+  find(word) {
+
+  }
+
+  delete(word) {
+
   }
 }
