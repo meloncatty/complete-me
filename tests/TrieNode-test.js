c9d9c528121374ce51494da6db68a447269a2203
meloncatty <k.handel66@gmail.com>
2018-04-03 21:12:12 -0600

    add trie.js functionality start tests for trie.js

--- tests/TrieNode-test.js
+++ tests/TrieNode-test.js
@@ -8,16 +8,23 @@ describe('TrieNode', () => {
     node = new Node('pizza')
   })
 
-  it('should be a thing', () => {
+  it('should exist', () => {
     expect(node).to.exist
-  })
-
-  it('should default next to null', () => {
-    expect(node.next).to.equal(null);
-    // expect(node.prev).to.equal(null);
   })
 
   it('should take value and assign it to val prop', () => {
     expect(node.val).to.equal('pizza')
   })
+
+  it('should have completeWord property as false when created', () => {
+    expect(node.completeWord).to.equal(false)
+  })
+
+  it('should have children property as empty object when created', () => {
+    expect(node.children).to.deep.equal({})
+  })
+
+  // it('should have zero prefixes when created', () => {
+  //   expect(node.prefixes).to.equal(0)
+  // })
 })
